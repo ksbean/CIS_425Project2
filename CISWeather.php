04acd3c65032250005bc0e62867e8bd6120cf128
petros-b <pbesieri@umich.edu>
2017-12-06 19:45:44 -0500

    #5 commit

--- CISWeather.php
+++ CISWeather.php
@@ -35,6 +35,7 @@ session_start();
   $placeinfo=$parsed_location2->{'results'};
   $dlt=$placeinfo[2]->{'geometry'}->{'location'}->{'lat'};
 <<<<<<< HEAD:FindBars.php
+<<<<<<< HEAD:FindBars.php
   $types=$placeinfo[2]->{'types'};
   
   
@@ -55,19 +56,6 @@ $count=0;
 //direction dist.
   $distance = file_get_contents("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=$dlt,$dlg&destinations=$lt,$lg&key=AIzaSyAHh5z_t5ophKURSNuyjBsSywIQivotQOU");
   $parsed_distance=json_decode($distance);
-  //echo $distance;
-//  echo $locationinfo;
-=======
-  
-  $json_string = file_get_contents("http://api.wunderground.com/api/aaad4f04f43767be/geolookup/conditions/q/$state/$city.json");
-  $parsed_json = json_decode($json_string);
-  $location = $parsed_json->{'location'}->{'city'};
-  $temp_f = $parsed_json->{'current_observation'}->{'temp_f'};
-  //echo "Current temperature in ${location} is: ${temp_f}\n";
-
-  $distance = file_get_contents("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=$dlt,$dlg&destinations=$lt,$lg&key=AIzaSyAHh5z_t5ophKURSNuyjBsSywIQivotQOU");
-  $parsed_distance=json_decode($distance);
-  //echo $distance;
   echo $locationinfo;
 >>>>>>> 0a5e891bb8138e2f66d8f0937ded1144e8d14aa1:CISWeather.php
   
