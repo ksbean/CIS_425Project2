5bb6f1f07fad45f695c602122439f06bf8b81a33
petros-b <pbesieri@umich.edu>
2017-12-06 19:34:11 -0500

    added weather determining php file

--- FindBars.php
+++ FindBars.php
@@ -1,23 +1,58 @@
 <?php
+session_start();
+
+
+// IDs your current location 
   $locationinfo = file_get_contents("https://ip.briantafoya.com/json");
+ 
   $parsed_location=json_decode($locationinfo);
+
   $city = $parsed_location->{'geodata'}->{'city'}->{'names'}->{'en'};
+  $_SESSION['cit']=$city;
+
   $Stateinfo=$parsed_location->{'geodata'}->{'subdivisions'};
+
   $state=$Stateinfo[0]->iso_code;
+  $_SESSION['state']=$state;
+
   $lt=$parsed_location->{'geodata'}->{'location'}->{'latitude'};
-  $lg=$parsed_location->{'geodata'}->{'location'}->{'longitude'};
+  $_SESSION['lat']=$lt;
+ 
+  $lg=$parsed_location->{'geodata'}->{'location'}->{'longitude'}; echo"<br>";
+  $_SESSION['long']=$lg;
   
-  $locationinfo2 = file_get_contents("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lt,$lg&radius=500&key=AIzaSyCbaWdlPCEYRrqggKX4kE_OVwddK0h1BpY");
+  
+//areas around you 
+
+  $radius=500;
+  $locationinfo2 = file_get_contents("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lt,$lg&radius=$radius&key=AIzaSyCbaWdlPCEYRrqggKX4kE_OVwddK0h1BpY");
   $parsed_location2=json_decode($locationinfo2);
   $placeinfo=$parsed_location2->{'results'};
-  $dlt=$placeinfo[1]->{'geometry'}->{'location'}->{'lat'};
-  $dlg=$placeinfo[1]->{'geometry'}->{'location'}->{'lng'};
+  $dlt=$placeinfo[2]->{'geometry'}->{'location'}->{'lat'};
+  $dlg=$placeinfo[2]->{'geometry'}->{'location'}->{'lng'};
+  $types=$placeinfo[2]->{'types'};
   
-
-
+  
+//temperature
   $json_string = file_get_contents("http://api.wunderground.com/api/aaad4f04f43767be/geolookup/conditions/q/$state/$city.json");
-  
+  $parsed_json = json_decode($json_string);
+  $location = $parsed_json->{'location'}->{'city'};
+  $temp_f = $parsed_json->{'current_observation'}->{'icon'};
+
+$length=count($placeinfo);
+$count=0;
+
+
+  echo "Current temperature in ${location} is: ${temp_f}";
+
+
+
+//direction dist.
   $distance = file_get_contents("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=$dlt,$dlg&destinations=$lt,$lg&key=AIzaSyAHh5z_t5ophKURSNuyjBsSywIQivotQOU");
   $parsed_distance=json_decode($distance);
-  echo $distance;
+  //echo $distance;
+//  echo $locationinfo;
+  
+  
+  
 ?>
---
+++ detEvent.php
@@ -0,0 +1,263 @@
+<?php
+
+session_start();
+
+$state=$_SESSION['state'];
+$city=$_SESSION['cit'];
+$json_string = file_get_contents("http://api.wunderground.com/api/aaad4f04f43767be/geolookup/conditions/q/$state/$city.json");
+$parsed_json = json_decode($json_string);
+$location = $parsed_json->{'location'}->{'city'};
+$temp_f = $parsed_json->{'current_observation'}->{'icon'};
+$temps=(int)$parsed_json->{'current_observation'}->{'temp_f'};
+$chnce_of_rain=(int)$parsed_json->{'current_observation'}->{'precip_metric_today'};
+
+if($temp_f=="partlycloudy")
+{
+    if($temps > 70 && $chnce_of_rain <=50)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+else if($temp_f=="sunny")
+{
+    if($temps > 70)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+else if($temp_f=="chanceflurries")
+{
+    if($temps > 32)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+else if($temp_f=="chancerain")
+{
+    if($temps > 70 && $chnce_of_rain <20)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+else if($temp_f=="chancesleet")
+{
+    if($temps >= 32 && $chnce_of_rain < 20)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+else if($temp_f=="chancesnow")
+{
+    if($temps > 32 &&)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+else if($temp_f=="chancestorms")
+{
+    if($temps > 70 && $chnce_of_rain<20)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+else if($temp_f=="clear")
+{
+    if($temps > 70)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+else if($temp_f=="cloudy")
+{
+    if($temps > 70 && $chnce_of_rain <=50)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+else if($temp_f=="flurries")
+{
+    if($temps > 32)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+else if($temp_f=="fog")
+{
+    if($temps > 70)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+else if($temp_f=="hazy")
+{
+    if($temps > 70  && $temps <=95)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+else if($temp_f=="mostlycloudy")
+{
+    if($temps > 70 && $chnce_of_rain <=50)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+else if($temp_f=="mostlysunny")
+{
+    if($temps > 70 && $temps <=95)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+else if($temp_f=="partlysunny")
+{
+    if($temps > 70 && $temps <=95)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+
+
+else if($temp_f=="snow")
+{
+    if($temps > 32)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+
+else if($temp_f=="sunny")
+{
+    if($temps > 70 && $temps <=95)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+else if($temp_f=="tstorms")
+{
+    if($temps > 70  && $chnce_of_rain <=50)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+else if($temp_f=="unkown")
+{
+    if($temps > 70)
+    {
+        //do outdoor shit 
+    }
+
+    else{
+        //do something else that's warmer
+    }
+}
+
+//using weather API to determine what place/thing to do based on location & radius
+
+//weather API to determine temp & condition 
+
+
+?>
